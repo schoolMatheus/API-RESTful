ad2088d59059b0bedf01bd8713e896549ceb5dae
schoolMatheus <160972647+schoolMatheus@users.noreply.github.com>
2024-11-14 11:24:57 -0300

    Pedido.java

--- atividade09/src/main/java/com/apimatheus/atividade09/domain/Pedido.java
+++ atividade09/src/main/java/com/apimatheus/atividade09/domain/Pedido.java
@@ -6,8 +6,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
 
 @Entity
+@Data
 public class Pedido {
     //Variáveis
     @Id
@@ -21,39 +23,10 @@ public class Pedido {
     private List<Long> id_Produtos;
     
     //Construtor
-    public Pedido() {}
-
-    public Pedido(Long id, Long id_Cliente, List<Long> id_Produtos) {
-        this.id = id;
-        this.id_Cliente = id_Cliente;
-        this.id_Produtos = id_Produtos;
-    }
 
     //GET
-    public Long getId() {
-        return id;
-    }
 
-    public Long getIdCliente() {
-        return id_Cliente;
-    }
-
-    public List<Long> getIdsProdutos() {
-        return id_Produtos;
-    }
-
-    //SET
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setIdCliente(Long idCliente) {
-        this.id_Cliente = idCliente;
-    }
-
-    public void setIdsProdutos(List<Long> idsProdutos) {
-        this.id_Produtos = idsProdutos;
-    }    
+    //SET  
 
     //Métodos
 
