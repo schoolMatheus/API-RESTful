40cad95ddeec5dc5ea4bc7cb914ddedb4c694cef
schoolMatheus <160972647+schoolMatheus@users.noreply.github.com>
2024-11-14 11:25:48 -0300

    Produto.java

--- atividade09/src/main/java/com/apimatheus/atividade09/domain/Produto.java
+++ atividade09/src/main/java/com/apimatheus/atividade09/domain/Produto.java
@@ -4,8 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
 
 @Entity
+@Data
 public class Produto {
    //Variáveis
    @Id
@@ -15,39 +17,10 @@ public class Produto {
    private Double preco;
    
    //Construtor
-   public Produto() {}
-
-   public Produto(Long id, String nome, Double preco) {
-   this.id = id;
-   this.nome = nome;
-       this.preco = preco;
-   }
 
    //GET
-   public Long getId() {
-       return id;
-   }
-
-   public String getNome() {
-       return nome;
-   }
-
-   public Double getPreco() {
-       return preco;
-   }
-
-   //SET
-   public void setId(Long id) {
-       this.id = id;
-   }
-
-   public void setNome(String nome) {
-       this.nome = nome;
-   }
-
-   public void setPreco(Double preco) {
-       this.preco = preco;
-   }
+ 
+   //SET  
    
    //Métodos
 
